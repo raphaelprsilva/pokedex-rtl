a1c6f362408dda6e3727f818ea0ffee6e7321a47
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-10-06 19:00:54 -0300

    test: add NotFound component tests

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Component <NotFound.js /> tests', () => {
+  test('renders a not found message', () => {
+    render(<NotFound />);
+
+    const notFoundMessage = screen.getByText(/Page requested not found/i);
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+
+  test('should renders an image', () => {
+    render(<NotFound />);
+
+    const notFoundImage = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(notFoundImage).toBeInTheDocument();
+  });
+
+  test('should have not found image must specific link', () => {
+    render(<NotFound />);
+
+    const notFoundImage = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(notFoundImage.src).toBe(url);
+  });
+});
