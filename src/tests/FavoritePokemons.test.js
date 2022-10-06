926949ac38da19aed6837a0c4f5146131df86ff6
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-10-06 18:54:20 -0300

    test: add FavoritePokemons component tests

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+
+import FavoritePokemons from '../components/FavoritePokemons';
+
+const pokemons = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+  },
+];
+
+describe('Tests <FavoritePokemons.js /> component', () => {
+  test('should render `No favorite pokemon found` message', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const messageElement = screen.getByText(/No favorite pokemon found/i);
+    expect(messageElement).toBeInTheDocument();
+  });
+
+  test('should render favorites pokemons', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    pokemons.forEach((pokemon) => {
+      const { name } = pokemon;
+      const nameElement = screen.getByText(name);
+      expect(nameElement).toBeInTheDocument();
+    });
+  });
+});
